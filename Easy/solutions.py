ab89e5ae74e92074f72473f72e7c71144b149fc0
Srinivash29 <srinivash.s_25am@kgkite.ac.in>
2026-07-22 14:02:45 +0530

    2nd easy problem was solved

--- Easy/solutions.py
+++ Easy/solutions.py
@@ -1,4 +1,4 @@
-#problem 1: 
+#problem no: 1
 
 class Solution(object):
     def twoSum(self, nums, target):
@@ -12,3 +12,15 @@ class Solution(object):
                 if(nums[i] + nums[j] == target):
                     return [i,j]
                     break
+#Problem no: 2
+class Solution(object):
+    def sumOfMultiples(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        a=0
+        for i in range(1,n+1):
+            if(i%3 == 0 or i%5 == 0 or i%7 == 0):
+                a+=i
+        return a  
