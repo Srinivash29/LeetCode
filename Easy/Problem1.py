92e27d8e37b79c4d584c68953425fef4f6ad6ec1
Srinivash29 <srinivash.s_25am@kgkite.ac.in>
2026-07-22 14:07:48 +0530

    problem no: 2652 was solved

--- Easy/Problem1.py
+++ Easy/Problem1.py
@@ -12,15 +12,3 @@ class Solution(object):
                 if(nums[i] + nums[j] == target):
                     return [i,j]
                     break
-#Problem no: 2
-class Solution(object):
-    def sumOfMultiples(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        a=0
-        for i in range(1,n+1):
-            if(i%3 == 0 or i%5 == 0 or i%7 == 0):
-                a+=i
-        return a  
