41a8f9fe63745d378fb38ab3132070b5bc318313
Srinivash29 <srinivash1329@gmail.com>
2026-08-01 18:33:50 +0530

    Time: 2 ms (26.03%), Space: 12.4 MB (54.71%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -4,11 +4,7 @@ class Solution(object):
         :type sentence: str
         :rtype: bool
         """
-        s = "abcdefghijklmnopqrstuvwxyz"
-        count = 0
-        for i in s:
-            if i in sentence:
-                count += 1
-        return(count == 26)
+        a = set(sentence)
+        return (len(a) == 26)
                 
         
