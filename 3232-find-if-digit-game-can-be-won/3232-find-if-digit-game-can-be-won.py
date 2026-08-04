a955ce051ccf60afcfd153c87323a36803c32192
Srinivash29 <srinivash1329@gmail.com>
2026-08-04 13:09:01 +0530

    Time: 0 ms (100%), Space: 12.3 MB (55.7%) - LeetHub

---
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def canAliceWin(self, nums):
+        a = 0
+        b = 0
+        for i in nums:
+            if(i < 10):
+                a += i
+            else:
+                b += i
+        if(a > b):
+            return True
+        elif(b > a):
+            return True
+        else:
+            return False                       
+
+
+
+
+
+
+           
+
+
+        
