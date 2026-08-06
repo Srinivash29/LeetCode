e85a037b56110861bd0060e0cdd1d1546e72fe07
Srinivash29 <srinivash1329@gmail.com>
2026-08-06 15:46:49 +0530

    Time: 3 ms (44.22%), Space: 12.4 MB (80.05%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def calPoints(self, operations):
+        """
+        :type operations: List[str]
+        :rtype: int
+        """
+        record = []
+        for i in range(len(operations)):
+            if operations[i] == "+":
+                record.append(record[-1] + record[-2])
+            elif operations[i] == "C":
+                record.pop()
+            elif operations[i] == "D":
+                record.append(record[-1] * 2)
+            else:
+                record.append(int(operations[i]))
+        return sum(record)           
+
+ 
+            
+
+
+
+
+
+        
+        
