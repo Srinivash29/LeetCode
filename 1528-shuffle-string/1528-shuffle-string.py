09284b52aa92339a12ef928654ddd3d7445e12b1
Srinivash29 <srinivash1329@gmail.com>
2026-08-20 15:16:04 +0530

    Time: 3 ms (43.18%), Space: 12.3 MB (57.09%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def restoreString(self, s, indices):
+        """
+        :type s: str
+        :type indices: List[int]
+        :rtype: str
+        """
+        a = []
+        b = [''] * len(s)
+        for i in s:
+            a.append(i)
+        for i in range(len(indices)):
+            b[indices[i]] = a[i]
+        return ''.join(b)    
+
+        
+            
+        
