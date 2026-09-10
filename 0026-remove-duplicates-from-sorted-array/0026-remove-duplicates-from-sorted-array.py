e3f3c519982fa6c3b891c44af6f89e5d47374e4d
Srinivash29 <srinivash1329@gmail.com>
2026-09-10 22:59:24 +0530

    Time: 0 ms (100%), Space: 13.9 MB (15.02%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -4,10 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        a = []
-        for i in range(len(nums)):
-            if nums[i] not in a:
-                a.append(nums[i])
-            for i in range(len(a)):
-                nums[i] = a[i]    
-        return len(a)  
+        k = 0
+        for i in range(1,len(nums)):
+            if nums[k] != nums[i]:
+                k += 1
+                nums[k] = nums[i]
+                
+        return k+1     
