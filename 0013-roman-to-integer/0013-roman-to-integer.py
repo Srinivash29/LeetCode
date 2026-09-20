d9e9a60dbfd32eaa3eefd8639574249420527eb1
Srinivash29 <srinivash1329@gmail.com>
2026-09-20 17:47:05 +0530

    Time: 9 ms (55.81%), Space: 12.3 MB (58.53%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -4,14 +4,15 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        result=0
-        roman={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-        for a,b in zip(s,s[1:]):
-            if roman[a]<roman[b]:
-                result-=roman[a]
+        result = 0
+        roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        l = len(s)
+        for i in range(l):
+            if i+1 < l and roman[s[i]] < roman[s[i+1]]:
+                result -= roman[s[i]]
             else:
-                result+=roman[a]
-        return result+roman[s[-1]]
-                                 
+                result += roman[s[i]]
+        return result
+
 
         
